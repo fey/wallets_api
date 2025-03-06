95e4dacec12cbf695071bc3bd3b8cffd92970260
Nikolay Gagarinov <feycot@gmail.com>
2025-03-07 00:26:35 +0500

    fmt

--- main.go
+++ main.go
@@ -14,43 +14,44 @@ import (
 type OperationType string
 
 const (
-    Deposit  OperationType = "DEPOSIT"
-    Withdraw OperationType = "WITHDRAW"
+	Deposit  OperationType = "DEPOSIT"
+	Withdraw OperationType = "WITHDRAW"
 )
 
 type WalletRequest struct {
-  WalletID     string  `json:"walletId"`      // ID кошелька
-  OperationType OperationType  `json:"operationType"` // Тип операции: DEPOSIT или WITHDRAW
-  Amount       float64 `json:"amount"`        // Сумма для операции
+	WalletID      string        `json:"walletId"`      // ID кошелька
+	OperationType OperationType `json:"operationType"` // Тип операции: DEPOSIT или WITHDRAW
+	Amount        float64       `json:"amount"`        // Сумма для операции
 }
 
 type Wallet struct {
-  ID      string  `json:"walletId"`
-  Balance float64 `json:"balance"`
+	ID      string  `json:"walletId"`
+	Balance float64 `json:"balance"`
 }
 
 var (
-  wallets = make(map[string]*Wallet)
-  mu sync.Mutex
+	wallets = make(map[string]*Wallet)
+	mu      sync.Mutex
 )
-//	@title			Wallet API
-//	@version		1.0
-//	@description	API для управления кошельками
-//	@host			localhost:8080
-//	@BasePath		/api/v1
+
+// @title			Wallet API
+// @version		1.0
+// @description	API для управления кошельками
+// @host			localhost:8080
+// @BasePath		/api/v1
 func main() {
-  http.Handle("/swagger/", httpSwagger.WrapHandler)
+	http.Handle("/swagger/", httpSwagger.WrapHandler)
+	http.HandleFunc("/api/v1/wallet", HandleWallet)
+	http.HandleFunc("/api/v1/wallets/", handleGetWallet)
 
-  http.HandleFunc("/api/v1/wallet", HandleWallet)
-  http.HandleFunc("/api/v1/wallets/", handleGetWallet)
-
-  fmt.Println("Starting server on :8080...")
-  if err := http.ListenAndServe(":8080", nil); err != nil {
-      log.Fatal(err)
-  }
+	fmt.Println("Starting server on :8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
 // handleWallet обрабатывает создание или обновление кошелька.
+//
 //	@Summary		Создание или обновление кошелька
 //	@Tags			wallet
 //	@ID				handleWallet
@@ -64,49 +65,50 @@ func main() {
 //	@Failure		400		{string}	string	"Invalid operation type"
 //	@Router			/wallet [post]
 func HandleWallet(w http.ResponseWriter, r *http.Request) {
-  if r.Method == http.MethodPost {
-      var req struct {
-          WalletID     string  `json:"walletId"`
-          OperationType string  `json:"operationType"`
-          Amount       float64 `json:"amount"`
-      }
+	if r.Method == http.MethodPost {
+		var req struct {
+			WalletID      string  `json:"walletId"`
+			OperationType string  `json:"operationType"`
+			Amount        float64 `json:"amount"`
+		}
 
-      if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-          http.Error(w, "Invalid request", http.StatusBadRequest)
-          return
-      }
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, "Invalid request", http.StatusBadRequest)
+			return
+		}
 
-      mu.Lock()
-      defer mu.Unlock()
+		mu.Lock()
+		defer mu.Unlock()
 
-      wallet, exists := wallets[req.WalletID]
-      if !exists {
-          wallet = &Wallet{ID: req.WalletID, Balance: 0}
-          wallets[req.WalletID] = wallet
-      }
+		wallet, exists := wallets[req.WalletID]
+		if !exists {
+			wallet = &Wallet{ID: req.WalletID, Balance: 0}
+			wallets[req.WalletID] = wallet
+		}
 
-      switch req.OperationType {
-      case string(Deposit):
-          wallet.Balance += req.Amount
-      case string(Withdraw):
-          if wallet.Balance < req.Amount {
-              http.Error(w, "Insufficient funds", http.StatusBadRequest)
-              return
-          }
-          wallet.Balance -= req.Amount
-      default:
-          http.Error(w, "Invalid operation type", http.StatusBadRequest)
-          return
-      }
+		switch req.OperationType {
+		case string(Deposit):
+			wallet.Balance += req.Amount
+		case string(Withdraw):
+			if wallet.Balance < req.Amount {
+				http.Error(w, "Insufficient funds", http.StatusBadRequest)
+				return
+			}
+			wallet.Balance -= req.Amount
+		default:
+			http.Error(w, "Invalid operation type", http.StatusBadRequest)
+			return
+		}
 
-      w.WriteHeader(http.StatusOK)
-      json.NewEncoder(w).Encode(wallet)
-  } else {
-      http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-  }
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(wallet)
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
 }
 
 // handleGetWallet обрабатывает запрос на получение информации о кошельке по его ID.
+//
 //	@Summary		Получение информации о кошельке
 //	@Description	Получает информацию о кошельке по его ID.
 //	@Tags			wallet
@@ -117,17 +119,17 @@ func HandleWallet(w http.ResponseWriter, r *http.Request) {
 //	@Failure		404			{string}	string	"Wallet not found"
 //	@Router			/wallets/{walletId} [get]
 func handleGetWallet(w http.ResponseWriter, r *http.Request) {
-  walletID := r.URL.Path[len("/api/v1/wallets/"):]
+	walletID := r.URL.Path[len("/api/v1/wallets/"):]
 
-  mu.Lock()
-  defer mu.Unlock()
+	mu.Lock()
+	defer mu.Unlock()
 
-  wallet, exists := wallets[walletID]
-  if !exists {
-      http.Error(w, "Wallet not found", http.StatusNotFound)
-      return
-  }
+	wallet, exists := wallets[walletID]
+	if !exists {
+		http.Error(w, "Wallet not found", http.StatusNotFound)
+		return
+	}
 
-  w.WriteHeader(http.StatusOK)
-  json.NewEncoder(w).Encode(wallet)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(wallet)
 }
