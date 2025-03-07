3ca0e2f1b5a675d702c92679ced2c9028673cf8e
Nikolay Gagarinov <feycot@gmail.com>
2025-03-08 01:41:12 +0500

    --wip-- [skip ci]

--- main.go
+++ main.go
@@ -76,83 +76,102 @@ func main() {
 
 		return c.SendString(greeting)
 	})
-	app.Get("/api/v1/wallets/:uuid", func (ctx *fiber.Ctx) error {
-	var err error
-	uuid := ctx.Params("uuid", "")
-	if uuid == "" {
-		return ctx.SendStatus(fiber.StatusNotFound)
-	}
-
-	var wallet Wallet
-	row := db.QueryRow("SELECT id, balance FROM wallets WHERE id = $1", uuid)
-
-	err = row.Scan(&wallet.WalletId, &wallet.Balance)
-
-	if err != nil {
-		if err == sql.ErrNoRows {
+	app.Get("/api/v1/wallets/:uuid", func(ctx *fiber.Ctx) error {
+		var err error
+		uuid := ctx.Params("uuid", "")
+		if uuid == "" {
 			return ctx.SendStatus(fiber.StatusNotFound)
 		}
-		return ctx.Status(fiber.StatusInternalServerError).SendString(err.Error())
-	}
 
-	jsonBytes, _ := json.Marshal(&wallet)
+		var wallet Wallet
+		row := db.QueryRow("SELECT id, balance FROM wallets WHERE id = $1", uuid)
 
-	return ctx.SendString(string(jsonBytes))
+		err = row.Scan(&wallet.WalletId, &wallet.Balance)
 
-	// return c.SendString(uuid)
-})
-	app.Post("/api/v1/wallets", func (ctx *fiber.Ctx) error {
-	var req WalletOperationRequest
-
-	if err := ctx.BodyParser(&req); err != nil {
-		return fmt.Errorf("body parser: %w", err)
-	}
-
-	errs := validateWalletOperation(req)
-
-	if (errs != nil) {
-		return ctx.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
-			"errors": errs,
-		})
-	}
-
-	var wallet Wallet
-	row := db.QueryRow("SELECT id, balance FROM wallets WHERE id = $1", req.WalletId)
-
-	err := row.Scan(&wallet.WalletId, &wallet.Balance)
-
-	if err != nil {
-		if err == sql.ErrNoRows {
-			return ctx.SendStatus(fiber.StatusNotFound)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				return ctx.SendStatus(fiber.StatusNotFound)
+			}
+			return ctx.Status(fiber.StatusInternalServerError).SendString(err.Error())
 		}
-		return ctx.Status(fiber.StatusInternalServerError).SendString(err.Error())
-	}
 
-	return ctx.JSON(wallet)
-})
+		jsonBytes, _ := json.Marshal(&wallet)
+
+		return ctx.SendString(string(jsonBytes))
+
+		// return c.SendString(uuid)
+	})
+	app.Post("/api/v1/wallets", func(ctx *fiber.Ctx) error {
+		var req WalletOperationRequest
+
+		if err := ctx.BodyParser(&req); err != nil {
+			return fmt.Errorf("body parser: %w", err)
+		}
+
+		errs := validateWalletOperation(req)
+
+		if errs != nil {
+			return ctx.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+				"errors": errs,
+			})
+		}
+
+		var wallet Wallet
+		row := db.QueryRow("SELECT id, balance FROM wallets WHERE id = $1", req.WalletId)
+
+		err := row.Scan(&wallet.WalletId, &wallet.Balance)
+
+		if err != nil {
+			if err == sql.ErrNoRows {
+				return ctx.SendStatus(fiber.StatusNotFound)
+			}
+			return ctx.Status(fiber.StatusInternalServerError).SendString(err.Error())
+		}
+
+		if req.OperationType == Deposit {
+			_, err := db.Exec("UPDATE wallets SET balance = balance + $1  WHERE id = $2", req.Amount, req.WalletId)
+			if err != nil {
+				return err
+			}
+			wallet.Balance += req.Amount
+		}
+
+		if req.OperationType == Withdraw {
+			_, err := db.Exec("UPDATE wallets SET balance = balance - $1  WHERE id = $2", req.Amount, req.WalletId)
+			if err != nil {
+				return err
+			}
+
+			wallet.Balance -= req.Amount
+		}
+
+		_, err = db.Exec("INSERT INTO transactions(wallet_id, operation_type, amount) VALUES($1, $2, $3)", req.WalletId, req.OperationType, req.Amount)
+		if err != nil {
+			return err
+		}
+
+		return ctx.JSON(wallet)
+	})
 	app.Get("/swagger/*", swagger.HandlerDefault)
 	log.Fatal(app.Listen(":8080"))
 }
 
-
-
-
 func validateWalletOperation(req WalletOperationRequest) []*ValidationError {
 	validate := validator.New(validator.WithRequiredStructEnabled())
 
-    var errors []*ValidationError
+	var errors []*ValidationError
 
-    err := validate.Struct(req)
-    if err != nil {
-        for _, err := range err.(validator.ValidationErrors) {
-            var el ValidationError
-            el.Field = err.Field()
-            el.Tag = err.Tag()
-            el.Value = err.Param()
-            errors = append(errors, &el)
-        }
-        return errors;
-    }
+	err := validate.Struct(req)
+	if err != nil {
+		for _, err := range err.(validator.ValidationErrors) {
+			var el ValidationError
+			el.Field = err.Field()
+			el.Tag = err.Tag()
+			el.Value = err.Param()
+			errors = append(errors, &el)
+		}
+		return errors
+	}
 
-		return nil
+	return nil
 }
